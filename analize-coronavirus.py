a727eed0ec1f7d9c9b53919f6427511f3256a365
Wyszynski <grzegorz.wyszynski@aptiv.com>
2020-03-18 11:52:47 +0100

    Added possibility to analize per country

--- analize-coronavirus.py
+++ analize-coronavirus.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import matplotlib.pylab as plt
+import cufflinks as cf
+from plotly.subplots import make_subplots
+from plotly import tools
 
-
+cf.go_offline()
 inData=pd.read_csv(r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
 
+modifiedData=inData.groupby('Country/Region').sum().transpose()
+
+dataOneCountry=[]
+for country in modifiedData.columns:
+    onlyMoreThan10Temp=modifiedData[modifiedData[country]>100][country].iloc[2:]
+    onlyMoreThan10Temp=onlyMoreThan10Temp.reset_index()[country]
+    dataOneCountry.append(onlyMoreThan10Temp.copy())
+    
+
+pd.DataFrame(dataOneCountry).dropna(how='all').transpose().iplot(yaxis_type='log')
+
+traces=[]
 pd.to_datetime(inData[inData['Country/Region']=='Poland'].transpose().iloc[4:].index)
 
 PolandData=inData[inData['Country/Region']=='Poland'].transpose().iloc[4:]
@@ -40,15 +55,15 @@ plt.show()
 
 
 SpainData=inData[inData['Country/Region']=='Spain'].transpose().iloc[4:]
-SpainData.index=pd.to_datetime(ItalyData.index)
-plt.plot(ItalyData[pd.to_datetime('2020-01-15'):],'*')
+SpainData.index=pd.to_datetime(SpainData.index)
+plt.plot(SpainData[pd.to_datetime('2020-01-15'):],'*')
 plt.xticks(rotation='vertical')
 plt.ylabel('Liczba zarażonych')
-plt.title('Włochy')
+plt.title('Hiszpania')
 plt.show()
 
-plt.semilogy(ItalyData[pd.to_datetime('2020-01-15'):],'*')
+plt.semilogy(SpainData[pd.to_datetime('2020-01-15'):],'*')
 plt.xticks(rotation='vertical')
 plt.ylabel('Liczba zarażonych')
-plt.title('Włochy - log plot')
+plt.title('Hiszpania - log plot')
 plt.show()
